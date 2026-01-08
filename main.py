4a2b1cfceb7dc6b72aac4ae17d7303bacb05bd89
FakeBees <jaredamron45@gmail.com>
2026-01-08 14:54:10 -0500

    Use env variables and create procfile

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import forms
+import os
 # Import your forms from the forms.py
 from forms import CreatePostForm, CommentForm
 
@@ -30,7 +31,8 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+print("SECRET_KEY =", app.config.get("SECRET_KEY"))
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -50,7 +52,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -237,4 +239,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
